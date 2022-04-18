cf880858aec909b701dcef246f4311e955edc81d
keeitm <kasia.malorny@gmail.com>
2022-04-18 22:02:33 +0200

    przerobiony kodzik

--- main.py
+++ main.py
@@ -1,33 +1,49 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-from scipy import integrate
-import ipywidgets as ipw
-alpha = 1.
-beta = 1.
-delta = 1.
-gamma = 1.
-x0 = 4.
-y0 = 2.
-def derivative (X, t, alpha, beta, deltam, gamma):
-    x, y = X
-    dotx = x * (alpha - beta *y)
-    doty = y * (-delta + gamma * x)
-    return np.array([dotx, doty])
-Nt = 1000
-tmax = 30.
-t = np.linspace(0.,tmax, Nt)
-X0 = [x0, y0]
-res = integrate.odeint(derivative, X0, t, args = (alpha, beta, delta, gamma))
-x, y = res.T
-(2, 1000)
-plt.figure()
-plt.grid()
-plt.title("odeint method")
-plt.plot(t, x, 'xb', label = 'Deer')
-plt.plot(t, y, '+r', label = "Wolves")
-plt.xlabel('Time t, [days]')
-plt.ylabel('Population')
-plt.legend()
+from scipy.integrate import odeint
 
+#ustawienie wartości początkowych
+y0=[10,1] # [ryby,niedzwiedzie] w tys.
+
+#ustawienie punktów czasowych
+t=np.linspace(0,50,num=1000)
+
+#ustawienie pararmetrów
+alpha= 1.1
+beta=0.4
+delta= 0.1
+gamma=0.4
+
+
+params = [alpha, beta, delta, gamma] #ułożenie kolejności parametrów
+
+def sim(variables, t, params):
+
+    x=variables[0] #poziom populacji ryb
+    y=variables[1] #poziom populacji niedźwiedzi
+
+
+    alpha = params[0]
+    beta = params[1]
+    delta = params[2]
+    gamma = params[3]
+
+    #zapisanie równań
+    dxdt=alpha*x-beta*x*y
+    dydt=delta * x*y-gamma *y
+
+    return([dxdt,dydt])
+
+y=odeint(sim, y0, t, args=(params,))
+
+f,(ax1,ax2)=plt.subplots(2)
+
+line1, = ax1.plot(t,y[:,0], color="b")
+line2, = ax2.plot(t,y[:,1], color="r")
+
+ax1.set_ylabel("Ryby (w tys.)")
+ax2.set_ylabel("Niedźwiedzie (w tys.)")
+ax2.set_xlabel("Czas")
+
+#wyswietlenie wyników
 plt.show()
