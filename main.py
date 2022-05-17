cba1e146eee6eef958dfab5d5854ee624a0a2b5d
keeitm <kasia.malorny@gmail.com>
2022-05-17 09:08:32 +0200

    opisy

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
-
+# ryby - niedzwiedzie
 #ustawienie wartości początkowych
-y0=[10,1] # [ryby,niedzwiedzie] w tys.
+y0=[10,1] # [ofiary,drapiezniki] w tys.
 
 #ustawienie punktów czasowych
 t=np.linspace(0,50,num=1000)
 
 #ustawienie pararmetrów
-alpha= 1.1
-beta=0.4
-delta= 0.1
-gamma=0.4
+alpha= 1.1 #wspólczynnik przyrostu ofiar
+beta=0.4 #częstośc umierania na skutek drapieżnictwa
+delta= 0.1 #wspólcynnik przyrostu ofiar
+gamma=0.4 #wspólczynnik umierania drapieżników
 
 
 params = [alpha, beta, delta, gamma] #ułożenie kolejności parametrów
 
 def sim(variables, t, params):
 
-    x=variables[0] #poziom populacji ryb
-    y=variables[1] #poziom populacji niedźwiedzi
+    x=variables[0] #poziom populacji ofiar
+    y=variables[1] #poziom populacji drapieznikow
 
 
     alpha = params[0]
@@ -47,3 +47,11 @@ ax2.set_xlabel("Czas")
 
 #wyswietlenie wyników
 plt.show()
+
+#stabilnośc modelu przeanalizować
+#portret fazowy sie zamknie
+#przeanalizowac dzialanie odelu dla zmieniajacych sie wspolczynnikow alfa, gamma, beta...
+#Model drapieżnik-ofiara - Model volterra lotki z modyfikacją....
+#zaimplemetowac roznice miedzy maximum (czas odradzania sie miedzy ofiarami)
+#wyroznic maksima i stworzyc wektor wyników
+#gui?
