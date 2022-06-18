d943fd9a3ddaaba618972042e2f2512ea4207378
ijabe <izabella.biesiada@gmail.com>
2022-06-19 01:44:43 +0200

    - modyfikacja ogarnięta (wykresy, portret fazowy, punkty stacjonarne) - uporządkowany kod

--- main.py
+++ main.py
@@ -1,109 +1,106 @@
-
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas
-from scipy import integrate
 from scipy.integrate import odeint
-import pylab as p
-# ryby - niedzwiedzie
-#ustawienie wartości początkowych
-liczofiar=10
-liczdrapiez=5
-y0=[liczofiar,liczdrapiez] # [ofiary,drapiezniki] w tys. warunki początkowe
 
-#ustawienie punktów czasowych
-t=np.linspace(0,100,num=1000)
+# ustawienie wartości początkowych
+liczofiar = 10
+liczdrapiez = 5
+y0 = [liczofiar, liczdrapiez]  # [ofiary,drapiezniki] w tys. warunki początkowe
 
-#ustawienie pararmetrów
-alpha= 0.9 #wspólczynnik przyrostu ofiar
-beta=0.4 #częstośc umierania ofiar na skutek drapieżnictwa (skuteczność polowań)
-delta= 0.1 #wspólczynnik przyrostu drapieżników (współczynnik rozrodczości drapieżników z uwzględnieniem drapieżnictwa)
-gamma=0.4 #wspólczynnik umierania drapieżników (śmiertelność w populacji drapieżników)
+# ustawienie punktów czasowych
+t = np.linspace(0, 110, num=1000)
+
+# ustawienie pararmetrów
+alpha = 1.1     # wspólczynnik przyrostu ofiar
+beta = 0.4      # częstośc umierania ofiar na skutek drapieżnictwa (skuteczność polowań)
+delta = 0.1    # wspólczynnik przyrostu drapieżników (współczynnik rozrodczości drapieżników z uwzględnieniem drapieżnictwa)
+gamma = 0.4     # wspólczynnik umierania drapieżników (śmiertelność w populacji drapieżników)
 
 
-params = [alpha, beta, delta, gamma] #ułożenie kolejności parametrów
+params = [alpha, beta, delta, gamma]    # ułożenie kolejności parametrów
 
 def sim(variables, t, params):
 
-    x=variables[0] #poziom populacji ofiar
-    y=variables[1] #poziom populacji drapieznikow
-
+    x = variables[0]    # poziom populacji ofiar
+    y = variables[1]    # poziom populacji drapieznikow
 
     alpha = params[0]
     beta = params[1]
     delta = params[2]
     gamma = params[3]
 
-    #zapisanie równań
-    dxdt=alpha*x-beta*x*y
-    dydt=delta * x*y-gamma *y
+    # zapisanie równań
+    dxdt = alpha*x-beta*x*y
+    dydt = delta * x*y-gamma * y
 
-    return([dxdt,dydt])
+    return([dxdt, dydt])
 
-yode=odeint(sim, y0, t, args=(params,))
-#############IZZZZZZZZZZZZZZZZZZZZ####################
-t=t.reshape(1,1000)
-wszystko=np.concatenate((yode,t.T), axis=1)
-wszystko=pandas.DataFrame(wszystko,columns=['ofiary','drapiezniki','czas'])
-maxo=max(wszystko['ofiary'])
-maxd=max(wszystko['drapiezniki'])
-idxmaxo=wszystko['ofiary'].idxmax()
-maxot = wszystko.loc[idxmaxo,'czas']
-idxmaxd=wszystko['drapiezniki'].idxmax()
-maxdt = wszystko.loc[idxmaxd,'czas']
-roznicat=abs(maxot-maxdt)
-mino=min(wszystko['ofiary'])
-mind=min(wszystko['drapiezniki'])
-print('Maksymalna liczba ofiar:',maxo)
-print('Minimalna liczba ofiar:',mino)
-print('Maksymalna liczba drapieżników:',maxd)
-print('Minimalna liczba drapieżników:',mind)
-print('Czas odnowienia populacji',roznicat)
 
-#czas odnawiania populacji
+yode = odeint(sim, y0, t, args=(params,))
+
+############# IZZZZZZZZZZZZZZZZZZZZA ####################
+t = t.reshape(1, 1000)
+wszystko = np.concatenate((yode, t.T), axis=1)
+wszystko = pandas.DataFrame(wszystko, columns=['ofiary', 'drapiezniki', 'czas'])
+maxo = max(wszystko['ofiary'])
+maxd = max(wszystko['drapiezniki'])
+idxmaxo = wszystko['ofiary'].idxmax()
+maxot = wszystko.loc[idxmaxo, 'czas']
+idxmaxd = wszystko['drapiezniki'].idxmax()
+maxdt = wszystko.loc[idxmaxd, 'czas']
+roznicat = abs(maxot-maxdt)
+mino = min(wszystko['ofiary'])
+mind = min(wszystko['drapiezniki'])
+print('Maksymalna liczba ofiar:', maxo)
+print('Minimalna liczba ofiar:', mino)
+print('Maksymalna liczba drapieżników:', maxd)
+print('Minimalna liczba drapieżników:', mind)
+print('Czas odnowienia populacji', roznicat)
+maxt = max(wszystko['czas'])
+# czas odnawiania populacji
 vvv = [0, 100]
-plt.plot(wszystko['czas'],wszystko['ofiary'], color="#6CABCD")
-plt.plot(wszystko['czas'],wszystko['drapiezniki'], color="#B2CD6C")
-plt.plot(maxot,maxo,"o",color="#41748F")
-
-plt.plot(maxdt,maxd,"o",color="#727F52")
-#wyswietlenie wyników
+plt.plot(wszystko['czas'], wszystko['ofiary'], color="#6CABCD")
+plt.plot(wszystko['czas'], wszystko['drapiezniki'], color="#B2CD6C")
+plt.plot(maxot, maxo, "o", color="#41748F")
+plt.plot(maxdt, maxd, "o", color="#727F52")
+# wyswietlenie wyników
 plt.autoscale(enable=True, axis='x', tight=True)
 plt.xlabel('Czas')
 plt.ylabel('Liczebność populacji [tys.]')
-plt.legend(['Ofiary','Drapieżniki'], loc='upper right')
+plt.legend(['Ofiary', 'Drapieżniki'], loc='upper right')
+plt.title('delta='+str(delta))
 plt.show()
-#przebieg czasowy
-vvv = [0, 100]
-plt.plot(wszystko['czas'],wszystko['ofiary'], color="#6CABCD")
-plt.plot(wszystko['czas'],wszystko['drapiezniki'], color="#B2CD6C")
-#wyswietlenie wyników
+# przebieg czasowy
+plt.plot(wszystko['czas'], wszystko['ofiary'], color="#6CABCD")
+plt.plot(wszystko['czas'], wszystko['drapiezniki'], color="#B2CD6C")
+# wyswietlenie wyników
 plt.autoscale(enable=True, axis='x', tight=True)
 plt.xlabel('Czas')
 plt.ylabel('Liczebność populacji [tys.]')
-plt.legend(['Ofiary','Drapieżniki'])
+plt.legend(['Ofiary', 'Drapieżniki'], loc='upper right')
+plt.title('czas='+str(maxt))
 plt.show()
-
-#portret fazowy
-
-N1=0
-P1=0
-N2=gamma/delta
-P2=alpha/beta
-
-plt.plot(N1,P1,'o',color='#F04BC5')
-plt.plot(N2,P2,'o',color='#A0F04B')
-plt.plot(yode[:,0],yode[:,1], color="#4BF0E6")
+# portret fazowy
+N1 = 0
+P1 = 0
+N2 = gamma/delta
+P2 = alpha/beta
+plt.plot(N1, P1, 'o', color='#B2CD6C')
+plt.plot(N2, P2, 'o', color='#AD1E60')
+plt.plot(yode[:, 0], yode[:, 1], color="#6CABCD")
 plt.xlabel('Drapieżniki')
 plt.ylabel('Ofiary')
-plt.legend(['Punkt stacjonarny niestabilny','Punkt stacjonarny stabilny'], loc='upper right')
-#line2, = ax1.plot(y2[:,0],y2[:,1], color="r", label="alpha = 0.9")
-
-#line3, = ax1.plot(y3[:,0],y3[:,1], color="g", label="alpha = 1.3")
-
-
-
+plt.title('delta='+str(delta))
+# plt.legend(['Punkt stacjonarny niestabilny', 'Punkt stacjonarny stabilny'], loc='upper right')
 plt.show()
+
+
+
+
+
+
+
 #stabilnośc modelu przeanalizować
 #portret fazowy sie zamknie
 #przeanalizowac dzialanie odelu dla zmieniajacych sie wspolczynnikow alfa, gamma, beta...
@@ -111,3 +108,6 @@ plt.show()
 #zaimplemetowac roznice miedzy maximum (czas odradzania sie miedzy ofiarami)
 #wyroznic maksima i stworzyc wektor wyników
 #gui?
+#dla jednego wspolczynnika jeszcze klasyczny
+#dla modyfikacji jeszcze wziac pod uwagę punkt stacjonarny i szczególnie dla pojemności (jak wpływają warunki początkowe)
+#stałe parametryu i zmieniamy pojemnośc środowiska
--- modyfikacja.py
+++ modyfikacja.py
@@ -1,27 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
-
-#ustawienie wartości początkowych
-y0=[10,1] # [ryby,niedzwiedzie] w tys.
+import pandas
+# ustawienie wartości początkowych
+liczofiar = 10
+liczdrapiez = 5
+y0 = [liczofiar, liczdrapiez]  # [ofiary,drapiezniki] w tys. warunki początkowe
 
 #ustawienie punktów czasowych
-t=np.linspace(0,110,num=1000)
+t = np.linspace(0, 110, num=1000)
 
 #ustawienie pararmetrów
-alpha= 1.1 # współczynnik
-beta=0.4
-delta= 0.1
-gamma=0.4
-K=100 # model z ograniczoną pojemnością środowiska dla ofiar
+alpha = 1.1  # współczynnik
+beta = 0.4
+delta = 0.1
+gamma = 0.4
+K = 100  # model z ograniczoną pojemnością środowiska dla ofiar
 
 
-params = [alpha, beta, delta, gamma, K] #ułożenie kolejności parametrów
+params = [alpha, beta, delta, gamma, K]  # ułożenie kolejności parametrów
 
 def sim(variables, t, params):
 
-    x=variables[0] #poziom populacji ryb
-    y=variables[1] #poziom populacji niedźwiedzi
+    x = variables[0]  # poziom populacji ofiar
+    y = variables[1]  # poziom populacji drapieżników
 
 
     alpha = params[0]
@@ -30,19 +32,69 @@ def sim(variables, t, params):
     gamma = params[3]
     K = params[4]
 
-    #zapisanie równań
+    # zapisanie równań
     dxdt=alpha*x*(1-(x/K))-beta*x*y
     dydt=delta * x*y-gamma *y
 
     return([dxdt,dydt])
 
-y=odeint(sim, y0, t, args=(params,))
 
-y=odeint(sim, y0, t, args=(params,))
+yode = odeint(sim, y0, t, args=(params,))
 
-v = [0, 110, 0, 20]
-plt.plot(t,y[:,0], color="m")
-plt.plot(t,y[:,1], color="c")
-plt.axis(v)
-#wyswietlenie wyników
+############# IZZZZZZZZZZZZZZZZZZZZA ####################
+t = t.reshape(1, 1000)
+wszystko = np.concatenate((yode, t.T), axis=1)
+wszystko = pandas.DataFrame(wszystko, columns=['ofiary', 'drapiezniki', 'czas'])
+maxo = max(wszystko['ofiary'])
+maxd = max(wszystko['drapiezniki'])
+idxmaxo = wszystko['ofiary'].idxmax()
+maxot = wszystko.loc[idxmaxo, 'czas']
+idxmaxd = wszystko['drapiezniki'].idxmax()
+maxdt = wszystko.loc[idxmaxd, 'czas']
+roznicat = abs(maxot-maxdt)
+mino = min(wszystko['ofiary'])
+mind = min(wszystko['drapiezniki'])
+print('Maksymalna liczba ofiar:', maxo)
+print('Minimalna liczba ofiar:', mino)
+print('Maksymalna liczba drapieżników:', maxd)
+print('Minimalna liczba drapieżników:', mind)
+print('Czas odnowienia populacji', roznicat)
+# czas odnawiania populacji
+plt.plot(wszystko['czas'], wszystko['ofiary'], color="#6CABCD")
+plt.plot(wszystko['czas'], wszystko['drapiezniki'], color="#B2CD6C")
+plt.plot(maxot, maxo, "o", color="#41748F"),
+plt.plot(maxdt, maxd, "o", color="#727F52")
+# wyswietlenie wyników
+plt.autoscale(enable=True, axis='x', tight=True)
+plt.xlabel('Czas')
+plt.ylabel('Liczebność populacji [tys.]')
+plt.legend(['Ofiary', 'Drapieżniki'], loc='upper right')
+plt.title('alpha='+str(alpha))
 plt.show()
+# przebieg czasowy
+plt.plot(wszystko['czas'], wszystko['ofiary'], color="#6CABCD")
+plt.plot(wszystko['czas'], wszystko['drapiezniki'], color="#B2CD6C")
+# wyswietlenie wyników
+plt.autoscale(enable=True, axis='x', tight=True)
+plt.xlabel('Czas')
+plt.ylabel('Liczebność populacji [tys.]')
+plt.legend(['Ofiary', 'Drapieżniki'])
+plt.title('alpha='+str(alpha))
+plt.show()
+# portret fazowy
+N1 = 0
+P1 = 0
+N2 = K
+P2 = 0
+N3 = gamma/delta
+P3 = (alpha/beta) * (1 - (gamma/(delta * K)))
+# plt.plot(N1, P1, 'o', color='#F04BC5')
+# plt.plot(N2, P2, 'o', color='#A0F04B')
+plt.plot(N3, P3, 'o', color='#FFC300')
+plt.plot(yode[:, 0], yode[:, 1], color="#4BF0E6")
+plt.xlabel('Drapieżniki')
+plt.ylabel('Ofiary')
+plt.title('alpha='+str(alpha))
+# plt.legend(['Punkt stacjonarny niestabilny', 'Punkt stacjonarny stabilny'], loc='upper right')
+plt.show()
+
